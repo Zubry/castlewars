f8b69bac8af55f73025cbf9c4825ee16cfae31e6
Zubry <zubryjs@gmail.com>
2016-12-20 00:50:39 -0500

    gg

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client');
 
-const socket = io.connect('http://localhost');
+const socket = io.connect();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
